dc2dd01359b56f1b5ac9ee5416a44d93e953f55e
abhinesh <abhinesh.c@eegrab.com>
2020-12-26 08:56:04 +0530

    first commit

---
+++ app/src/main/java/com/eegrab/wishkeyui/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.eegrab.wishkeyui
+
+
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.login_screen.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_screen)
+
+        setSupportActionBar(mytoolbar)
+
+
+
+    }
+}
